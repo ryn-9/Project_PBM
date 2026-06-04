3866b22814a4310a9fe13c2387231a0cf6eef0bc
ryn-9 <mbinroy@gmail.com>
2026-06-04 12:46:50 +0700

    resolve conflict, fix profil page, dan  update beranda

--- lib/page/profil_page.dart
+++ lib/page/profil_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../service/authService.dart';
-import '../auth/login_page.dart';
+import '../service/authService.dart';
+import 'auth/login_page.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
